202a0a90ac4e21a1d5e3334ac7febadac9af997a
Adonis Gaitatzis <backupbrain@gmail.com>
2020-09-03 10:22:43 +0200

    preparing for pypi

--- setup.py
+++ setup.py
@@ -18,8 +18,11 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+
     ],
     python_requires='>=3.6',
 )
